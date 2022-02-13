9837b9a42396ab6bf047368dca592252fe635a48
unknown <patn97@gmail.com>
2022-02-13 14:01:16 -0500

    remove calls to prepareData

--- module/documents/actor.js
+++ module/documents/actor.js
@@ -35,8 +35,78 @@
   
       // Make separate methods for each Actor type (character, npc, etc.) to keep
       // things organized.
-      this._prepareCharacterData(actorData);
-      this._prepareNpcData(actorData);
+      //this._prepareCharacterData(actorData);
+      //this._prepareNpcData(actorData);
     }
- 
-}
+  
+    /**
+     * Prepare Character type specific data
+     */
+    _prepareCharacterData(actorData) {
+      if (actorData.type !== 'character') return;
+  
+      // Make modifications to data here. For example:
+      const data = actorData.data;
+  
+      // Loop through ability scores, and add their modifiers to our sheet output.
+      for (let [key, ability] of Object.entries(data.abilities)) {
+        // Calculate the modifier using d20 rules.
+        ability.mod = Math.floor((ability.value - 10) / 2);
+      }
+    }
+  
+    /**
+     * Prepare NPC type specific data.
+     */
+    _prepareNpcData(actorData) {
+      if (actorData.type !== 'npc') return;
+  
+      // Make modifications to data here. For example:
+      const data = actorData.data;
+      data.xp = (data.cr * data.cr) * 100;
+    }
+  
+    /**
+     * Override getRollData() that's supplied to rolls.
+     */
+    getRollData() {
+      const data = super.getRollData();
+  
+      // Prepare character roll data.
+      this._getCharacterRollData(data);
+      this._getNpcRollData(data);
+  
+      return data;
+    }
+  
+    /**
+     * Prepare character roll data.
+     */
+    _getCharacterRollData(data) {
+      if (this.data.type !== 'character') return;
+  
+      // Copy the ability scores to the top level, so that rolls can use
+      // formulas like `@str.mod + 4`.
+      if (data.abilities) {
+        for (let [k, v] of Object.entries(data.abilities)) {
+          data[k] = foundry.utils.deepClone(v);
+        }
+      }
+  
+      // Add level for easier access, or fall back to 0.
+      if (data.attributes.level) {
+        data.lvl = data.attributes.level.value ?? 0;
+      }
+    }
+  
+    /**
+     * Prepare NPC roll data.
+     */
+    _getNpcRollData(data) {
+      if (this.data.type !== 'npc') return;
+  
+      // Process additional NPC data here.
+    }
+  
+  }
+  
